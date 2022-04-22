03b46d7c5f806a9e1a2bbd620c8fe326102c00e0
Jihun Park <jihunpark.tech@gmail.com>
2022-04-22 21:32:43 +0900

    add: 컴퓨터는 1에서 9까지 서로 다른 임의의 수 3개를 랜덤으로 선택

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -1,10 +1,26 @@
 package baseball;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class BaseballGame {
 
     public static void start() {
 
         InputView inputView = new InputView();
         ResultView resultView = new ResultView();
+
+        Integer[] computerNumber = getComputerNumber();
+    }
+
+    private static Integer[] getComputerNumber() {
+        Set<Integer> numbers = new HashSet<>();
+
+        while (numbers.size() < 3) {
+            double num = Math.random() * 9 + 1;
+            numbers.add((int) num);
+        }
+
+        return numbers.toArray(new Integer[3]);
     }
 }
